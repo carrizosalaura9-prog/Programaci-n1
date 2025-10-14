999acb79c10bb1396571212e19fca855fec445a0
Laura Carrizosa <carrizosalaura9@gmail.com>
2025-10-14 08:35:26 -0700

    PRACTICA AREA

---
+++ practica07/calculadora_rectangulos.py
@@ -0,0 +1,15 @@
+
+print()
+print("-*CALCULAR EL AREA DE UN RECTANGULO*-")
+print()
+
+base=5
+altura=10
+
+def calcular_area(base,altura):
+    
+    return base*altura
+resultado=calcular_area(base,altura)
+
+print(f"El area del rectangulo es: {resultado}")
+print()
